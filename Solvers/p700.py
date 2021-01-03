eee4c1f75567347dfd0146e37b52efa8f613310e
yihongme <yihong703@gmail.com>
2021-01-04 00:25:32 +0800

    solve problem 700

--- Solvers/p700.py
+++ Solvers/p700.py
@@ -1,4 +1,3 @@
-from tqdm import tqdm
 import numpy as np
 '''
 With Mathematica
@@ -11,23 +10,12 @@ FindInstance[
 def fastlinearcongruence(powx, divmodx, N, withstats=False):
     x, y, z = egcditer(powx, N, withstats)
     answer = (y * divmodx) % N
-    if withstats == True:
-        print(f"answer = {answer}, mrbt = {a}, mrst = {b},  mranswer = {_testx(N, answer)}")
+
     if x > 1:
         powx //= x
         divmodx //= x
         N //= x
-        if withstats == True:
-            print(f"powx = {powx}, divmodx = {divmodx}, N = {N}")
-        x, y, z = egcditer(powx, N, withstats)
-        if withstats == True:
-            print(f"x = {x}, y = {y}, z = {z}")
-        answer = (y * divmodx) % N
-        if withstats == True:
-            print(f"answer = {answer}, mrbt = {a}, mrst = {b},  mranswer = {_testx(N, answer)}")
     answer = (y * divmodx) % N
-    if withstats == True:
-        print(f"answer = {answer}, mrbt = {a}, mrst = {b},  mranswer = {_testx(N, answer)}")
     return answer
 
 
@@ -37,23 +25,17 @@ def egcditer(a, b, withstats=False):
     old_s = 1
     old_r = a
     quotient = 0
-    if withstats == True:
-        print(f"quotient = {quotient}, old_r = {old_r}, r = {r}, old_s = {old_s}, s = {s}")
+
     while r != 0:
         quotient = old_r // r
         old_r, r = r, old_r - quotient * r
         old_s, s = s, old_s - quotient * s
-        if withstats == True:
-            print(f"quotient = {quotient}, old_r = {old_r}, r = {r}, old_s = {old_s}, s = {s}")
+
     if b != 0:
         bezout_t = quotient = (old_r - old_s * a) // b
-        if withstats == True:
-            print(f"bezout_t = {bezout_t}")
+
     else:
         bezout_t = 0
-    if withstats == True:
-        print("Bézout coefficients:", (old_s, bezout_t))
-        print("greatest common divisor:", old_r)
     return old_r, old_s, bezout_t
 
 
